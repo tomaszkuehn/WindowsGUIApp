82bca0af14a3b6bf2f75e8ff1a515a08ed43b775
Tomasz Kuehn <tomasz.kuehn@gmail.com>
2022-08-17 23:36:29 +0200

    Adding animated shape and textured object

--- examples/example_win32_directx10/myApp.cpp
+++ examples/example_win32_directx10/myApp.cpp
@@ -57,6 +57,53 @@ namespace myApp {
             ImGui::SetCursorPosX(200.0f);
             ImGui::Text("NUM %d", num);
 
+            ImGuiIO& io = ImGui::GetIO();
+            ImGuiStyle& style = ImGui::GetStyle();
+            ImDrawList* draw_list = ImGui::GetWindowDrawList();
+            static float x = 0;
+            x = x + 1;
+            const ImU32 color = 0x80ffffff;
+            draw_list->AddTriangleFilled(ImVec2(100 + x, 100),
+                ImVec2(100 + x, 200),
+                ImVec2(200 + x, 100),
+                color);
+            ImGui::ShowMetricsWindow();
+            ImGui::TextWrapped(
+                "Below we are displaying the font texture (which is the only texture we have access to in this demo). "
+                "Use the 'ImTextureID' type as storage to pass pointers or identifier to your own texture data. "
+                "Hover the texture for a zoomed view!");
+            ImTextureID my_tex_id = io.Fonts->TexID;
+            float my_tex_w = (float)io.Fonts->TexWidth;
+            float my_tex_h = (float)io.Fonts->TexHeight;
+            {
+                ImGui::Text("%.0fx%.0f", my_tex_w, my_tex_h);
+                ImVec2 pos = ImGui::GetCursorScreenPos();
+                ImVec2 uv_min = ImVec2(0.0f, 0.0f);                 // Top-left
+                ImVec2 uv_max = ImVec2(1.0f, 1.0f);                 // Lower-right
+                ImVec4 tint_col = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);   // No tint
+                ImVec4 border_col = ImVec4(1.0f, 1.0f, 1.0f, 0.5f); // 50% opaque white
+                ImGui::Image(my_tex_id, ImVec2(my_tex_w, my_tex_h), uv_min, uv_max, tint_col);// , border_col);
+                if (ImGui::IsItemHovered())
+                {
+                    ImGui::BeginTooltip();
+                    float region_sz = 32.0f;
+                    float region_x = io.MousePos.x - pos.x - region_sz * 0.5f;
+                    float region_y = io.MousePos.y - pos.y - region_sz * 0.5f;
+                    float zoom = 4.0f;
+                    if (region_x < 0.0f) { region_x = 0.0f; }
+                    else if (region_x > my_tex_w - region_sz) { region_x = my_tex_w - region_sz; }
+                    if (region_y < 0.0f) { region_y = 0.0f; }
+                    else if (region_y > my_tex_h - region_sz) { region_y = my_tex_h - region_sz; }
+                    ImGui::Text("Min: (%.2f, %.2f)", region_x, region_y);
+                    ImGui::Text("Max: (%.2f, %.2f)", region_x + region_sz, region_y + region_sz);
+                    ImVec2 uv0 = ImVec2((region_x) / my_tex_w, (region_y) / my_tex_h);
+                    ImVec2 uv1 = ImVec2((region_x + region_sz) / my_tex_w, (region_y + region_sz) / my_tex_h);
+                    ImGui::Image(my_tex_id, ImVec2(region_sz * zoom, region_sz * zoom), uv0, uv1, tint_col, border_col);
+                    ImGui::EndTooltip();
+                }
+            }
+
+            
         }
         ImGui::End();
     }
